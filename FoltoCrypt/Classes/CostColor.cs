e1c2093854f70d7c45a39c8336441ef4e6efcb84
YooPita <YooPita@users.noreply.github.com>
2018-01-20 22:45:41 +0300

    Class "Cost to color converter"

---
+++ FoltoCrypt/Classes/CostColor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace FoltoCrypt.Classes
+{
+    class CostColor
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                SolidColorBrush color;
+                var a = (string)value;
+                var a1 = a.Split(' ')[0];
+                var b = Double.Parse(a1);
+                if (b < 0)
+                {
+                    color = new SolidColorBrush(Colors.Red);
+                }
+                else if (b > 0)
+                {
+                    color = new SolidColorBrush(Colors.Green);
+                }
+                else
+                {
+                    color = new SolidColorBrush(Colors.Black);
+                }
+                return color;
+            }
+            catch
+            {
+                return new SolidColorBrush(Colors.Black);
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
